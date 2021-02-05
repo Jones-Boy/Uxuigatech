c09e6b5d6c9837a498d8054284ee05a77f167438
Jones-Boy <Djyoungjones@gmail.com>
2021-02-05 18:24:44 -0500

    Updated page

---
+++ 00-MyCode/Copy of 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,49 @@
+//function addNumbers() {
+    function addnumbers() {
+var two = 2;
+var three = 3;
+var five = two + three;
+console.log(five);
+}
+addnumbers();
+
+
+// Create the stringConcat function here.
+function stringConcat() {
+    var taco = "Taco";
+    var cat = "Cat";
+    var flow
+}
+    var taco = "Taco"; 
+    var cat = "Cat";
+    var flow 
+
+    Im_that_dude = Micahel Jones 
+
+
+var disney = "Snow White";
+var stockCount = 10;
+var isSleeping = true;
+
+var stockCount = 10;
+var stockCount = 10 + 5;
+var stockCount = anotherVariable;
+var stockCount = +=5;
+
+console.log("Hello,World!");
+
+function myFunction(number) {
+    number +=
+    console.log(number);
+}
+
+myFunction(5);
+
+
+
+
+
+
+
+
+
